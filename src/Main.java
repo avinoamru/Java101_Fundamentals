568907edec232c642569e697f027ddb17e7ff606
Avinoam Ruach <ruachavinoam@gmail.com>
2019-11-13 16:13:24 -0800

    displaying aguments length, arguments and if there are no arguments, Hello World!

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,30 @@
+import java.util.Scanner;
 
 public class Main {
 
 	
+	
 
 	
-	    public static void main(String args[]) {
-	        for (int i = 0; i < args.length; i++)
-	            System.out.println("args[" + i + "]: " + args[i]);
+	    public static void main(String[] args) {
+	 
+	    	if (args.length > 0) {
+	    		
+				System.out.println("The arguments length is: " + args.length);
+	    		for (int i = 0; i < args.length; i++) {
+					System.out.println("The arguments are: " + args[i]);
+				}
+				
+				
+			}
+	    	else {
+				System.out.println("Hello World!");
+			}
+	    	
+	    	
+	    	
 	    }
-	}
+	    
+}
+	
 	
