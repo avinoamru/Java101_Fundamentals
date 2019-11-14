4cebaa7ce99f312ea78ddee43b068df42a4fa8b6
Avinoam Ruach <ruachavinoam@gmail.com>
2019-11-13 16:50:19 -0800

    added String, int and printing them

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import java.util.Scanner;
 
 public class Main {
 
@@ -7,12 +6,18 @@ public class Main {
 
 	
 	    public static void main(String[] args) {
-	 
+	    	
+	    	String java_String = "JAVA is hard Q_Q";
+	    	int aNumber = 100000;
+	    	
+	    	
+	    	
 	    	if (args.length > 0) {
 	    		
-				System.out.println("The arguments length is: " + args.length);
+				System.out.println("The arguments length is: " + args.length + "\nThe arguments are: ");
+				
 	    		for (int i = 0; i < args.length; i++) {
-					System.out.println("The arguments are: " + args[i]);
+					System.out.println( args[i]);
 				}
 				
 				
@@ -21,9 +26,14 @@ public class Main {
 				System.out.println("Hello World!");
 			}
 	    	
+	    	System.out.println("This is the int variable -> " + aNumber + "\nThis is the String variable -> " + java_String);
 	    	
 	    	
 	    }
+	    	
+	    	
+	    	
+	    
 	    
 }
 	
