dbbede1c03967647a764c54c383386a4f710dcd0
Avinoam Ruach <ruachavinoam@gmail.com>
2019-11-14 10:59:04 -0800

    added list of variables and print them

--- src/Main.java
+++ src/Main.java
@@ -9,9 +9,16 @@ public class Main {
 	    	
 	    	String java_String = "JAVA is hard Q_Q";
 	    	int aNumber = 100000;
+	    	
 	    	final int finalNumber = 12345;
-	    	
-	    	
+	    	byte small_number = 127;
+	    	short bigger_than_byte = 32_767;
+	    	int bigger_than_short = 2_147_483_647;
+	    	long THE_BIGGEST_NUMBER = 9_223_372_036_854_775_807L;
+	    	float small_float = 2_147_483_647.123454321234f;
+	    	double big_float = 9_223_372_036_854_775_807.1234321234321;
+	    	boolean true_or_false = true;
+	    	char a_character = 'A';
 	    	
 	    	if (args.length > 0) {
 	    		
@@ -35,7 +42,8 @@ public class Main {
 	    	System.out.println(java_String);
 	    	java_String = java_String + ("" + aNumber);
 	    	System.out.println("\n\nThis time I'm printing the final variable (in this case it's a number) and assigning a variable to another one in the same time:\nFirst the final number: " + finalNumber + "\nand this is the String variable I had which I added a number to it: " + java_String + ".");
-	    	
+	    	System.out.println("\n\n This is a list of variables I need to display:"
+	    			+  "\nbyte - " + small_number + "\nshort - " + bigger_than_byte + "\nint - " + bigger_than_short + "\nlong - " + THE_BIGGEST_NUMBER + "\nfloat - " + small_float + "\ndouble - " + big_float + "\nboolean - " + true_or_false + "\nchar - " + a_character);
 	    	
 	    }
 	    	
