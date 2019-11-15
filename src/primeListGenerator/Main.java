fb76e1a2fe7cd83b71b9fb821a6ee815c111d7b2
Avinoam Ruach <ruachavinoam@gmail.com>
2019-11-15 13:49:13 -0800

    prime list generator init

---
+++ src/primeListGenerator/Main.java
@@ -0,0 +1,24 @@
+package primeListGenerator;
+
+public class Main {
+
+	public Main(int aNumber) {
+		
+		for (int i = 0; i < aNumber; i++) {
+			
+			if (i % 2 == 0) {
+				
+			}
+		}
+		
+	}
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		
+	}
+	
+}
---
+++ src/primeListGenerator/PrimeNumber.java
@@ -0,0 +1,13 @@
+package primeListGenerator;
+
+public class PrimeNumber {
+
+	public PrimeNumber(int aNumber) {
+		
+		boolean is_it_prime = false;
+
+		if (aNumber % 2 == 0) {
+			
+		}
+	}
+}
